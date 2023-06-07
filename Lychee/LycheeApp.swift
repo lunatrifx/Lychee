49bb6c63b27120f4c52b86d6d74867c5bca359f9
lunatrifx <pnickqlim@gmail.com>
2023-06-07 11:56:00 -0700

    Initial Commit

---
+++ Lychee/LycheeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LycheeApp.swift
+//  Lychee
+//
+//  Created by Paul Dominick Limpin on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct LycheeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
